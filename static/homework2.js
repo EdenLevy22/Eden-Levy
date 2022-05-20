ca8ffee61ce07d7a178f3e2d4aa11a8d2a0c2629
EdenLevy22 <103043688+EdenLevy22@users.noreply.github.com>
2022-05-20 19:01:40 +0300

    bla bla

---
+++ static/homework2.js
@@ -0,0 +1,6 @@
+function msgprint() {  
+    alert("You are Successfully Called the JavaScript function");  
+ }  
+ function myFunction() {
+   document.getElementById("demo").innerHTML = "Paragraph changed.";
+ }
